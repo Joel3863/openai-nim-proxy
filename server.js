dd59c243ef4947909dc0c7bfcfc58f7b5f001e33
Joel3863 <kaka3863@proton.me>
2025-10-03 09:28:27 +0200

    server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.all('*', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, 0.0.0.0, () => {
   console.log(`OpenAI to NVIDIA NIM Proxy running on port ${PORT}`);
   console.log(`Health check: http://localhost:${PORT}/health`);
 });
